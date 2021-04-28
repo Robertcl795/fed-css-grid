2112021538117543c8c21d78371491f38c9057f7
robertcl795 <robertcl795.rc@gmail.com>
2021-04-28 01:54:51 -0500

    add first layout

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	import { Router } from 'svelte-routing';
+	import Navbar from './layout/Navbar.svelte';
+	import Footer from './layout/Footer.svelte';
+
+	export let url = "";
+</script>
+
+<main>
+	<Router url="{url}">
+		<div id="stars1"></div>
+		<div id="stars2"></div>
+		<div id="stars3"></div>
+		<Navbar class="main--navbar"/>
+		<section class="main--content">
+
+		</section>
+		<Footer class="main--footer"/>
+	</Router>
+</main>
+
+<style type="text/scss">
+	@import './styles/main.scss';
+	main {
+		max-height: calc(100vh - 2rem);
+		margin: 1rem;
+
+		display: grid;
+		grid-template-areas: "navbar" "page-content" "footer";
+		grid-template-columns: 1fr;
+		:global(main > .main--navbar) {
+			grid-area: navbar;
+		}
+		:global(main > .page--content) {
+			grid-area: page--content;
+		}
+		:global(main > .main--footer) {
+			grid-area: footer;
+		}
+	}
+</style>
---
+++ src/layout/Navbar.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { Link } from 'svelte-routing'; 
+</script>
+
+<style type="text/scss">
+    .navbar--main {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        > .navbar--links {
+            display: flex;
+            gap: 1rem;
+        }
+        img {
+            height: 3rem;
+            width: 4rem;
+        }
+    }
+</style>
+
+<nav class="navbar--main" >
+    <Link class="navbar--link" to="/"><img src="logo.png" alt="FED@IBM Logo"/></Link>
+    <ul class="navbar--links">
+        <li><Link class="navbar--link" to="/movies">Movies</Link></li>
+        <li><Link class="navbar--link" to="/characters">Characters</Link></li>
+        <li><Link class="navbar--link" to="/lightsaber">Lightsaber Lab</Link></li>
+        <li><Link class="navbar--link" to="/ships-gallery">Ships Gallery</Link></li>
+    </ul>
+</nav>
